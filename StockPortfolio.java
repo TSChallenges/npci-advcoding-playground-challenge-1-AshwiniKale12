9ce70c13de08ade89bae3c2860ffb0af90f8325c
Ashwini Dattatray Kale <100211594+AshwiniKale12@users.noreply.github.com>
2025-04-14 17:39:43 +0000

    Completed playground challenge

--- StockPortfolio.java
+++ StockPortfolio.java
@@ -1,29 +1,36 @@
 class StockPortfolio {
-    // TODO: Add necessary fields for stock details
+    
     private String symbol;
     private int quantity;
     private double buyingPrice;
     private double currentPrice;
+    private double highestPrice;
 
-    // TODO: Initialize stock details
+
+
     public StockPortfolio(String symbol, int quantity, double buyingPrice) {
-        // TODO
+        this.symbol = symbol;
+        this.quantity = quantity;
+        this.buyingPrice = buyingPrice;
+        this.currentPrice = buyingPrice;
+        this.highestPrice = buyingPrice;
     }
 
-    // TODO: Update current market price
     public void updatePrice(double newPrice) {
-        // TODO
+        if(newPrice > 0){
+            currentPrice = newPrice;
+        }
+        if(newPrice > highestPrice){
+            highestPrice = newPrice;
+        }
     }
-
-    // TODO: Calculate total profit or loss
+   
     public double calculateProfit() {
-        // TODO
-        return 0.0;
+       return (currentPrice - buyingPrice) * quantity;
+       
     }
 
-    // TODO: Calculate current total value of holdings
     public double getCurrentValue() {
-        // TODO
-        return 0.0;
+     return currentPrice * quantity;
     }
 }
